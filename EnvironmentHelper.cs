e181a7358fd9c4ab5a9a6090d7082dca7adfe515
Tinyfish <tifish@gmail.com>
2025-03-27 20:40:40 +0800

    Add safe mode and reboot functions to EnvironmentHelper.

--- EnvironmentHelper.cs
+++ EnvironmentHelper.cs
@@ -1,4 +1,6 @@
-﻿namespace JeekTools;
+﻿using System.Runtime.InteropServices;
+
+namespace JeekTools;
 
 public static class EnvironmentHelper
 {
@@ -40,5 +42,40 @@ public static class EnvironmentHelper
         }
 
         return result;
+    }
+
+    internal const int SM_CLEANBOOT = 67;
+
+    [DllImport("user32.dll")]
+    private static extern int GetSystemMetrics(int smIndex);
+
+    public static bool IsSafeMode()
+    {
+        return GetSystemMetrics(SM_CLEANBOOT) != 0;
+    }
+
+    public static void SetBootToSafeMode()
+    {
+        Admin.StartElevated("bcdedit.exe", "/set {current} safeboot Minimal");
+    }
+
+    public static void SetBootToNormalMode()
+    {
+        Admin.StartElevated("bcdedit.exe", "/deletevalue {current} safeboot");
+    }
+
+    public static void ShutdownSystem()
+    {
+        Admin.StartElevated("shutdown.exe", "/s /t 0");
+    }
+
+    public static void RebootSystem()
+    {
+        Admin.StartElevated("shutdown.exe", $"/r /t 0");
+    }
+
+    public static void HibernateSystem()
+    {
+        Admin.StartElevated("shutdown.exe", "/h");
     }
 }
