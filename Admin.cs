4c1564828d600c490340379612da6c94064bc82c
Tinyfish <tifish@gmail.com>
2025-06-04 17:00:23 +0800

    Add Admin.StartElevatedAndWait()

--- Admin.cs
+++ Admin.cs
@@ -12,14 +12,38 @@ public static class Admin
         return principal.IsInRole(WindowsBuiltInRole.Administrator);
     }
 
-    public static void StartElevated(string fileName, string arguments = "")
+    public static Process? StartElevated(string fileName, string arguments = "")
     {
-        Process.Start(new ProcessStartInfo
+        return Process.Start(new ProcessStartInfo
         {
             FileName = fileName,
             Arguments = arguments,
             UseShellExecute = true,
             Verb = "runas",
         });
+    }
+
+    public static async Task<string> StartElevatedAndWait(string fileName, string arguments = "")
+    {
+        var process = Process.Start(new ProcessStartInfo
+        {
+            FileName = fileName,
+            Arguments = arguments,
+            UseShellExecute = false,
+            CreateNoWindow = true,
+            Verb = "runas",
+            RedirectStandardError = true,
+            RedirectStandardOutput = true,
+        });
+
+        if (process == null)
+            return "";
+
+        await process.WaitForExitAsync();
+
+        if (process.ExitCode == 0)
+            return "";
+
+        return process.StandardError.ReadToEnd();
     }
 }
