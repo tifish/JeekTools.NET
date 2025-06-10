38b37147004f89de6312ab904a4be2ee66fc48de
Tinyfish <tifish@gmail.com>
2025-06-10 15:52:33 +0800

    Add Ssd.IsSsd()

---
+++ Ssd.cs
@@ -0,0 +1,21 @@
+﻿using System.Management;
+
+namespace JeekTools;
+
+public static class Ssd
+{
+    private static readonly Dictionary<char, bool> _isSsdCache = [];
+
+    public static bool IsSsd(char driveLetter)
+    {
+        driveLetter = char.ToUpper(driveLetter);
+
+        if (_isSsdCache.TryGetValue(driveLetter, out var isSsd))
+            return isSsd;
+
+        isSsd = new DriveInfo(driveLetter.ToString() + ":\\").IsSsd();
+        _isSsdCache[driveLetter] = isSsd;
+
+        return isSsd;
+    }
+}
