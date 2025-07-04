64bda0b12fd3d532ede4cdd3f5b5ff9284ac97c6
Tinyfish <tifish@gmail.com>
2025-07-04 21:54:57 +0800

    Add new modules.

---
+++ GitHubMirrors.cs
@@ -0,0 +1,14 @@
+﻿namespace JeekTools;
+
+public static class GitHubMirrors
+{
+    public static string[] GetMirrors(string url)
+    {
+        return
+        [
+            url,
+            url.Replace("https://github.com/", "https://ghfast.top/https://github.com/"),
+            url.Replace("https://github.com/", "https://gh-proxy.com/github.com/")
+        ];
+    }
+}
---
+++ Nssm.cs
@@ -0,0 +1,72 @@
+﻿using System.Diagnostics;
+
+namespace JeekTools;
+
+public static class Nssm
+{
+    public static string NssmPath { get; set; } = Path.Join(AppContext.BaseDirectory, "Nssm", "nssm.exe");
+
+    private static Process? CallNssm(string command)
+    {
+        return Process.Start(new ProcessStartInfo(NssmPath, command)
+        {
+            RedirectStandardOutput = true,
+            UseShellExecute = false,
+            CreateNoWindow = true,
+        });
+    }
+
+    private static async Task<bool> CallNssmAndWait(string command)
+    {
+        var process = CallNssm(command);
+        if (process is null)
+            return false;
+        await process.WaitForExitAsync();
+        return process.ExitCode == 0;
+    }
+
+    public static async Task<bool> InstallService(string serviceName, string fileName, string arguments)
+    {
+        return await CallNssmAndWait($"""install "{serviceName}" "{fileName}" {arguments}""");
+    }
+
+    public static async Task<bool> UninstallService(string serviceName)
+    {
+        return await CallNssmAndWait($"""remove "{serviceName}" confirm""");
+    }
+
+    public static async Task<bool> StartService(string serviceName)
+    {
+        return await CallNssmAndWait($"""start "{serviceName}""");
+    }
+
+    public static async Task<bool> StopService(string serviceName)
+    {
+        return await CallNssmAndWait($"""stop "{serviceName}""");
+    }
+
+    public static async Task<bool> RestartService(string serviceName)
+    {
+        return await CallNssmAndWait($"""restart "{serviceName}""");
+    }
+
+    public static async Task<ServiceStatus> GetServiceStatus(string serviceName)
+    {
+        var statusString = (await Executor.RunWithOutput(NssmPath, $"""status "{serviceName}" """)).Trim();
+        return statusString switch
+        {
+            "" => ServiceStatus.None,
+            "SERVICE_STOPPED" or "SERVICE_PAUSED" or "SERVICE_STOP_PENDING" => ServiceStatus.Stopped,
+            _ => ServiceStatus.Running,
+        };
+    }
+
+}
+
+public enum ServiceStatus
+{
+    None,
+    Stopped,
+    Running,
+}
+
