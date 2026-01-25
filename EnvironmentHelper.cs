4191da9d3e546e57b73a52c13bd5d19e35b33b35
Tinyfish <tifish@gmail.com>
2026-01-25 13:10:05 +0800

    Use FindInPath() in DotNetRunCmd package.

--- EnvironmentHelper.cs
+++ EnvironmentHelper.cs
@@ -4,46 +4,6 @@ namespace JeekTools;
 
 public static class EnvironmentHelper
 {
-    public static string FindInPath(string fileName)
-    {
-        var path = Environment.GetEnvironmentVariable("PATH");
-        if (path == null)
-            return "";
-
-        foreach (var dir in path.Split(';'))
-        {
-            if (dir == "")
-                continue;
-
-            var filePath = Path.Combine(dir, fileName);
-            if (File.Exists(filePath))
-                return filePath;
-        }
-
-        return "";
-    }
-
-    public static List<string> FindAllInPath(string fileName)
-    {
-        var result = new List<string>();
-
-        var path = Environment.GetEnvironmentVariable("PATH");
-        if (path == null)
-            return result;
-
-        foreach (var dir in path.Split(';'))
-        {
-            if (dir == "")
-                continue;
-
-            var filePath = Path.Combine(dir, fileName);
-            if (File.Exists(filePath))
-                result.Add(filePath);
-        }
-
-        return result;
-    }
-
     internal const int SM_CLEANBOOT = 67;
 
     [DllImport("user32.dll")]
